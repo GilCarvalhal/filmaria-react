acb7c162e9e4876038206741a164c81b397aaa4d
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-06-29 22:06:10 -0300

    criando o header

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+function Header() {
+    return (
+        <header>
+            <Link className='logo' to={'/'}>
+                Prime Flix
+            </Link>
+
+            <Link className='favoritos' to={'/favoritos'}>
+                Meus filmes
+            </Link>
+        </header>
+    )
+}
+
+export default Header;
