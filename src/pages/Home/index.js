38786ee64d574ab11a8b9282d284348e9804b75b
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-07-01 20:20:24 -0300

    listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from '../../services/api';
+import { Link } from "react-router-dom";
+import './home.css';
 
 function Home() {
     const [filmes, setFilmes] = useState([]);
@@ -15,7 +17,8 @@ function Home() {
                 }
             });
 
-            console.log(response.data.results);
+            // console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 10));
         }
 
         loadFilmes();
@@ -23,8 +26,22 @@ function Home() {
     }, [])
 
     return (
-        <div>
-            BEM VINDO A HOME!
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return (
+                        <article>
+                            <strong key={filme.id}>
+                                {filme.title}
+                            </strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>
+                                Acessar
+                            </Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
