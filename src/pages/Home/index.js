051cab93d0386395dba0a564fbc4a89c1b08617d
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-06-30 21:14:52 -0300

    criando nossa home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import api from '../../services/api';
+
 function Home() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+
+        async function loadFilmes() {
+            const response = await api.get("/movie/now_playing", {
+                params: {
+                    api_key: '65c5a8c3fbf30cb8934edc85464da2dd',
+                    language: "pt-BR",
+                    page: 1,
+                }
+            });
+
+            console.log(response.data.results);
+        }
+
+        loadFilmes();
+
+    }, [])
+
     return (
         <div>
             BEM VINDO A HOME!
