5dd40ba6de46ef6dc2b730f0134dcd72ffa82d90
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-07-01 23:39:56 -0300

    detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,60 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from '../../services/api';
+
 function Filme() {
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: '65c5a8c3fbf30cb8934edc85464da2dd',
+                    language: "pt-BR",
+                }
+            })
+                .then((response) => {
+                    setFilme(response.data);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    console.log("FILME NÃO ENCONTRADO!");
+                })
+        }
+        loadFilme();
+
+        return () => {
+            console.log("COMPONENTE FOI DESMONTADO");
+        }
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="filme-info">
+                <h1>
+                    Carregando detalhes...
+                </h1>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            BEM VINDO A PÁGINA DETALHES DO FILME
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>
+                Sinopse
+            </h3>
+            <span>
+                {filme.overview}
+            </span>
+            <strong>
+                Avaliação: {filme.vote_average} / 10
+            </strong>
         </div>
     )
 }
